8845b90054477c409adddaf9b3a607866ed0e622
Gonçalo Quesado 25868 <goncaloquesado@gmail.com>
2023-02-17 12:03:15 +0000

    Commit 4

--- app/src/main/java/com/example/firstaplication/MainActivity.kt
+++ app/src/main/java/com/example/firstaplication/MainActivity.kt
@@ -2,7 +2,7 @@ package com.example.firstaplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-//BRANCH 1 PUSH1
+//BRANCH 1 PUSH2
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {

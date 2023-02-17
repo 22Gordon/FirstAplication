a5986958d750624f127301c7739b41769d621237
22Gordon <61708088+22Gordon@users.noreply.github.com>
2023-02-17 11:55:21 +0000

    Update MainActivity.kt

--- app/src/main/java/com/example/firstaplication/MainActivity.kt
+++ app/src/main/java/com/example/firstaplication/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.firstaplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+//Teste de pull
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {

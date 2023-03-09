7e8e7987b90ebcdc4c1bd0dd5813f359c3f89102
Gonçalo Quesado 25868 <goncaloquesado@gmail.com>
2023-03-09 17:39:26 +0000

    Commit 10 -Logs life cycle

--- app/src/main/java/com/example/firstaplication/MainActivity.kt
+++ app/src/main/java/com/example/firstaplication/MainActivity.kt
@@ -2,11 +2,39 @@ package com.example.firstaplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+
 //Master push
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d("****TAG", "onCreate")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("****TAG", "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("****TAG", "onStop")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("****TAG", "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("****TAG", "onResume")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("****TAG", "onDestroy")
     }
 }
